5823d104cec5980977c78809c8208cb9ce09962a
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-07-27 08:10:23 -0400

    Added new app and update pet-clinic

---
+++ receipewebapp/src/main/java/com/lavalliere/daniel/spring/receipewebapp/ReceipewebappApplication.java
@@ -0,0 +1,13 @@
+package com.lavalliere.daniel.spring.receipewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceipewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceipewebappApplication.class, args);
+	}
+
+}
---
+++ receipewebapp/src/test/java/com/lavalliere/daniel/spring/receipewebapp/ReceipewebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavalliere.daniel.spring.receipewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceipewebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
