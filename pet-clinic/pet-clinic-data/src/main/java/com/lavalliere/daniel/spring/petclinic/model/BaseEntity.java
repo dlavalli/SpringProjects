6393088c33fd9b2e4a3afb145e6de4fea311f8a6
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-08-13 17:14:51 -0400

    Added lombok to pet-clinic

--- pet-clinic/pet-clinic-data/src/main/java/com/lavalliere/daniel/spring/petclinic/model/BaseEntity.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/lavalliere/daniel/spring/petclinic/model/BaseEntity.java
@@ -1,23 +1,20 @@
 package com.lavalliere.daniel.spring.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
 @MappedSuperclass   // Tell JPA other class will inherit this, we do not need this mapped to a specific database
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // Other values are TABLE, SEQUENCE, AUTO
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
--- pet-clinic/pet-clinic-data/src/main/java/com/lavalliere/daniel/spring/petclinic/model/Pet.java
+++ pet-clinic/pet-clinic-data/src/main/java/com/lavalliere/daniel/spring/petclinic/model/Pet.java
@@ -1,10 +1,17 @@
 package com.lavalliere.daniel.spring.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -16,53 +23,13 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "type_id")
     private PetType petType;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet", fetch = FetchType.EAGER)
     private Set<Visit> visits = new HashSet<>();
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Set<Visit> getVisits() {
-        return visits;
-    }
-
-    public void setVisits(Set<Visit> visits) {
-        this.visits = visits;
-    }
 }
