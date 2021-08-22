fdb3a78d80b85e4425ea4efdd442ea2d1f162b5e
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-08-22 13:48:58 -0400

    Enabled junit 5 test

---
+++ pet-clinic/pet-clinic-web/src/test/java/com/lavalliere/daniel/spring/petclinic/PetClinicApplicationTest.java
@@ -0,0 +1,25 @@
+package com.lavalliere.daniel.spring.petclinic;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PetClinicApplicationTest {
+
+	@BeforeEach
+	public void setup() {
+	}
+
+	@AfterEach
+	public void cleanup() {
+	}
+
+	@Test
+	void contextLoads() {
+	}
+}
