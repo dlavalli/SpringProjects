0ba7f1526f4d1ab669cd2f0014ad33fd913e18be
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-08-31 16:25:50 -0400

    Added code to update ingredients

--- receipewebapp/src/main/java/com/lavalliere/daniel/spring/recipewebapp/services/IngredientService.java
+++ receipewebapp/src/main/java/com/lavalliere/daniel/spring/recipewebapp/services/IngredientService.java
@@ -5,4 +5,5 @@ import com.lavalliere.daniel.spring.recipewebapp.commands.IngredientCommand;
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
 }
