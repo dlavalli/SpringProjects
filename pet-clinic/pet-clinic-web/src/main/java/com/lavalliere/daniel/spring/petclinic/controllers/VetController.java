d0d6519bec7ed195ddb1b3228d169f966731c0e0
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-10-03 15:48:10 -0400

    Updated petclinic

--- pet-clinic/pet-clinic-web/src/main/java/com/lavalliere/daniel/spring/petclinic/controllers/VetController.java
+++ pet-clinic/pet-clinic-web/src/main/java/com/lavalliere/daniel/spring/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.lavalliere.daniel.spring.petclinic.controllers;
 
+import com.lavalliere.daniel.spring.petclinic.model.Vet;
 import com.lavalliere.daniel.spring.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +25,12 @@ public class VetController {
 
         // Send back to thymeleaf the template filename (ie: index.html)
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
--- pet-clinic/pet-clinic-web/src/test/java/com/lavalliere/daniel/spring/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic/pet-clinic-web/src/test/java/com/lavalliere/daniel/spring/petclinic/controllers/OwnerControllerTest.java
@@ -90,6 +90,21 @@ class OwnerControllerTest {
     }
 
     @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(
+                        Owner.builder().id(1l).build(),
+                        Owner.builder().id(2l).build())
+                );
+
+        mockMvc.perform(get("/owners")
+                .param("lastName",""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));;
+    }
+
+    @Test
     void displayOwners() throws  Exception {
         Owner owner = Owner.builder().build();
         owner.setId(1L);
