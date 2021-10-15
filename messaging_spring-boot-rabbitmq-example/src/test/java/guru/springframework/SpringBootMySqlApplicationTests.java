af3b6bb804027632c85e4a66ec60768d1b8603cf
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-10-15 07:38:43 -0400

    Added more extended RabbitMQ example application

---
+++ messaging_spring-boot-rabbitmq-example/src/test/java/guru/springframework/SpringBootMySqlApplicationTests.java
@@ -0,0 +1,19 @@
+package guru.springframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMySqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		assertTrue(true);
+	}
+
+}
