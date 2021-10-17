d8ba4e77ebcdfaabd41b6fd2c8a19f406b3349b5
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-10-17 10:16:49 -0400

    Latest update to Jackson samples

--- JacksonSamples/src/main/java/com/lavalliere/daniel/spring/jackson/SampleJacksonApplication.java
+++ JacksonSamples/src/main/java/com/lavalliere/daniel/spring/jackson/SampleJacksonApplication.java
@@ -1,6 +1,5 @@
 package com.lavalliere.daniel.spring.jackson;
 
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
@@ -47,6 +47,53 @@ class Car {
     private String color;
 }
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+class Apple {
+    // Jackson rules: your fields need to be public
+    // or they need to have public getter / setters
+
+    @JsonIgnore
+    private String countryOfOrigin;
+    private String color;
+    public int quantity;
+}
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+class Banana {
+    // Jackson rules: your fields need to be public
+    // or they need to have public getter / setters
+
+    private String countryOfOrigin;
+    private String color;
+
+    @JsonIgnore
+    public int quantity = 4;
+
+    public boolean inSeason = true;
+}
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+class OtherCar {
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @EqualsAndHashCode
+    static class Engine {  // Gives error if not declared as static
+        private int cylinder;
+        private int horsepower;
+    }
+    private String model;
+    private Engine engine;
+}
 
 public class SampleJacksonApplication {
 
@@ -103,6 +150,27 @@ public class SampleJacksonApplication {
         Car car = mapper.readValue(jsonString, Car.class);
         System.out.println(String.format("Car brandName: %s Type: %s Color:%s\n", car.getBrand(), car.getModel(), car.getColor()));
 
-        // Jackson Annotations  @JsonIgnore: de-serialization -
+        // Jackson Annotations  @JsonIgnore: de-serialization
+        // Allows to ignore fields in Java object during serialization
+        Apple apple = new Apple("United States", "red", 10);
+        jsonString = mapper.writeValueAsString(apple);
+        System.out.println(jsonString);
+
+        // Jackson Annotations  @JsonIgnore: de-serialization
+        // Allows to ignore fields in Java object during de-serialization
+        jsonString = "{ \"countryOfOrigin\": \"Brazil\", \"color\": \"yellow\", \"quantity\": 20}";
+        Banana banana = mapper.readValue(jsonString, Banana.class);
+        System.out.println(String.format("Banana countryOfOrigin: %s color: %s quantity: %d\n",
+                banana.getCountryOfOrigin(), banana.getColor(), banana.getQuantity()));
+
+        // Serialization - 	• Java Objects within java Objects
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);  // For pretty printing
+        OtherCar ocar = new OtherCar("Ford F150", new OtherCar.Engine(2, 775));
+        jsonString = mapper.writeValueAsString(ocar);
+        System.out.println(jsonString);
+
+        // De-Serialization - 	• Java Objects within java Objects
+        OtherCar ocar2 = mapper.readValue(jsonString, OtherCar.class);
+        System.out.println(String.format("Object serialize/deserialized are equal: %s\n", ocar2.equals(ocar) ? "true" : "false"));
     }
 }
