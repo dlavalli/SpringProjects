e8b6968fd9098fec6791cc3558ff53cbdd75dce2
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-11-05 08:46:06 -0400

    rest mvc added create cutomer with post

--- rest-mvc/src/main/java/guru/springfamework/api/v1/mapper/CustomerMapper.java
+++ rest-mvc/src/main/java/guru/springfamework/api/v1/mapper/CustomerMapper.java
@@ -9,6 +9,6 @@ import org.mapstruct.factory.Mappers;
 public interface CustomerMapper {
 
     CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
-
     CustomerDTO customerToCustomerDTO(Customer customer);
+    Customer customerDtoToCustomer(CustomerDTO customerDTO);
 }
