9e3b2a1157974ece5ebd950385fda339dae3894f
Daniel Lavalliere <d.lavalliere@videotron.ca>
2021-11-12 13:06:15 -0500

    Updated rest mvc controllers and services

--- rest-mvc/src/main/java/guru/springfamework/controllers/v1/CategoryController.java
+++ rest-mvc/src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -6,13 +6,14 @@ import guru.springfamework.services.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/api/v1/categories/")
+// @Controller
+@RestController
+@RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
+
+    public static final String BASE_URL = "/api/v1/categories";
 
     private final CategoryService categoryService;
 
@@ -20,17 +21,34 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    /*
+    // Requires @Controller
     @GetMapping
     public ResponseEntity<CatorgoryListDTO> getallCatetories(){
-
         return new ResponseEntity<CatorgoryListDTO>(
                 new CatorgoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
     }
+     */
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public CatorgoryListDTO getallCatetories() {
+       return new CatorgoryListDTO(categoryService.getAllCategories());
+    }
+
+    /*
+    // Requires @Controller
     @GetMapping("{name}")
     public ResponseEntity<CategoryDTO> getCategoryByName( @PathVariable String name){
         return new ResponseEntity<CategoryDTO>(
                 categoryService.getCategoryByName(name), HttpStatus.OK
         );
+    }
+     */
+
+    @GetMapping("{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName( @PathVariable String name) {
+        return categoryService.getCategoryByName(name);
     }
 }
--- rest-mvc/src/test/java/guru/springfamework/controllers/v1/CustomerControllerTest.java
+++ rest-mvc/src/test/java/guru/springfamework/controllers/v1/CustomerControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springfamework.controllers.v1;
 
 import guru.springfamework.api.v1.model.CustomerDTO;
+import guru.springfamework.controllers.RestResponseEntityExceptionHandler;
 import guru.springfamework.services.CustomerService;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +18,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -36,7 +37,9 @@ public class CustomerControllerTest extends AbstractRestControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(customerController)
+                .setControllerAdvice(new RestResponseEntityExceptionHandler())
+                .build();
     }
 
     @Test
@@ -147,5 +150,15 @@ public class CustomerControllerTest extends AbstractRestControllerTest {
                 .andExpect(jsonPath("$.firstname", equalTo("Fred")))
                 .andExpect(jsonPath("$.lastname", equalTo("Flintstone")))
                 .andExpect(jsonPath("$.customer_url", equalTo("/api/v1/customers/1")));
+    }
+
+    @Test
+    public void testDeleteCustomer() throws Exception {
+
+        mockMvc.perform(delete("/api/v1/customers/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(customerService).deleteCustomerById(anyLong());
     }
 }
