8b18497b7dd289e99aeffde968da3cbb1865c7e6
Daniel Lavalliere <d.lavalliere@videotron.ca>
2025-04-21 21:26:34 -0400

    REST mvc added optional instead of throwing

--- spring-6-rest-mvc/src/test/java/com/lavalliere/daniel/spring/spring6restmvc/controller/CustomerControllerTest.java
+++ spring-6-rest-mvc/src/test/java/com/lavalliere/daniel/spring/spring6restmvc/controller/CustomerControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -131,7 +132,7 @@ class CustomerControllerTest {
     void getCustomerById() throws Exception {
         Customer testCustomer = customerServiceImpl.listCustomers().get(0);
 
-        given(customerService.getCustomerById(testCustomer.getId())).willReturn(testCustomer);
+        given(customerService.getCustomerById(testCustomer.getId())).willReturn(Optional.of(testCustomer));
 
         mockMvc.perform(
                 get(CustomerController.CUSTOMER_PATH_ID,testCustomer.getId())
