32f8e30513b2bd3d1e38fbf2686afa744535ee55
Daniel Lavalliere <d.lavalliere@videotron.ca>
2025-05-05 14:00:27 -0400

    Added missing update for OAuth2 resource server (OAuth2ResourceServer branch)

--- spring-6-rest-mvc/src/main/java/com/lavalliere/daniel/spring/spring6restmvc/Spring6RestMvcApplication.java
+++ spring-6-rest-mvc/src/main/java/com/lavalliere/daniel/spring/spring6restmvc/Spring6RestMvcApplication.java
@@ -1,9 +1,13 @@
 package com.lavalliere.daniel.spring.spring6restmvc;
 
+import com.lavalliere.daniel.spring.spring6restmvc.mappers.BeerMapperImpl;
+import com.lavalliere.daniel.spring.spring6restmvc.mappers.CustomerMapperImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({BeerMapperImpl.class, CustomerMapperImpl.class})
 public class Spring6RestMvcApplication {
 
 	public static void main(String[] args) {
