bab2844facd666e9c794c68495756f568649caa7
Daniel Lavalliere <d.lavalliere@videotron.ca>
2025-05-05 11:14:22 -0400

    Added spring 6 OAuth2 authentication server (default implementation)

---
+++ spring-6-auth-server/src/test/java/com/lavalliere/daniel/spring/spring6authserver/Spring6AuthServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lavalliere.daniel.spring.spring6authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+
+@SpringBootTest
+class Spring6AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
