5085d4b960aef7957513c498b24adfc9e98c4d9f
Daniel Lavalliere <d.lavalliere@videotron.ca>
2025-05-15 09:33:36 -0400

    Added thymeleaf template to web-mvc app sample

--- spring-6-web-mvc/src/main/java/com/lavalliere/daniel/spring/webmvc/ConferenceConfig.java
+++ spring-6-web-mvc/src/main/java/com/lavalliere/daniel/spring/webmvc/ConferenceConfig.java
@@ -1,5 +1,7 @@
 package com.lavalliere.daniel.spring.webmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -10,11 +12,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 import java.util.Locale;
 
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -45,4 +53,38 @@ public class ConferenceConfig implements WebMvcConfigurer {
         lci.setParamName("lang");
         return lci;
     }
+
+
+    /*
+    // THE NORMAL WAY TO FIND A TEMPLATE RESOLVER
+    // WITHOUT THE CURRENT JSP ENABLED SETUP
+
+    // Add Thymeleaf template resolver to find the template that we want to use
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    // Create a template engine to serve up the templates that we are going to use
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+        engine.setEnableSpringELCompiler(true);
+        return engine;
+    }
+
+    // Create a view resolver to display everything one we have found them, used them
+    @Bean
+    public ViewResolver thymeleafResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(0);
+        return viewResolver;
+    }
+    */
 }
