f315e2161269ce4a32c9242ea446680ace8ccc37
Daniel Lavallier <d.lavalliere@videotron.ca>
2026-04-27 12:03:32 -0400

    Added missing files from Spring AI functions

---
+++ spring-ai-functions/spring-ai-functions/src/test/java/com/lavalliere/daniel/spring/springaifunctions/SpringAiFunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavalliere.daniel.spring.springaifunctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiFunctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
