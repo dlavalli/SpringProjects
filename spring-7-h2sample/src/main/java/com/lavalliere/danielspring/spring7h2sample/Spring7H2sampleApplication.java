04e6fca7e1992603f0fb024bfcf58716bd651837
Daniel Lavalliere <d.lavalliere@videotron.ca>
2026-08-18 16:09:19 -0400

    Added Spring 7 H2 console example (no DB)

---
+++ spring-7-h2sample/src/main/java/com/lavalliere/danielspring/spring7h2sample/Spring7H2sampleApplication.java
@@ -0,0 +1,13 @@
+package com.lavalliere.danielspring.spring7h2sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7H2sampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring7H2sampleApplication.class, args);
+    }
+
+}
---
+++ spring-7-h2sample/src/test/java/com/lavalliere/danielspring/spring7h2sample/Spring7H2sampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavalliere.danielspring.spring7h2sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7H2sampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
